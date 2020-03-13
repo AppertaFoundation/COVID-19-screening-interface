b3a421e25312ba233eda515f86b3faf64f4600a1
Nick Booker <nick.booker@opusvl.com>
2020-03-13 14:24:34 +0000

    Set correct widget type for clinical author {id,name}
    Didn't use underlying CharField as that requires a maximum length,
    but I don't want to restrict that arbitrarily just to suit the API.
    So we have to use TextField and override the widget in the admin form.

--- c19-backend/api/admin.py
+++ c19-backend/api/admin.py
@@ -1,9 +1,24 @@
+from django import forms
 from django.contrib import admin
 
 # Register your models here.
 from api.models import C19APIUserProfile
 
+class C19APIUserProfileAdminForm(forms.ModelForm):
+    class Meta:
+        model = C19APIUserProfile
+        fields = (
+            'user',
+            'clinical_author_name',
+            'clinical_author_id',
+        )
+        widgets = {
+            'clinical_author_name': forms.TextInput(),
+            'clinical_author_id': forms.TextInput(),
+        }
+
+
 class C19APIUserProfileAdmin(admin.ModelAdmin):
-    pass
+    form = C19APIUserProfileAdminForm
 
 admin.site.register(C19APIUserProfile, C19APIUserProfileAdmin)
