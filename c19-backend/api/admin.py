eec965a24d355034b635566fbce3ffb4c556a4e2
Nick Booker <nick.booker@opusvl.com>
2020-03-13 14:14:09 +0000

    Add user profile with clinical author information

--- c19-backend/api/admin.py
+++ c19-backend/api/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+from api.models import C19APIUserProfile
+
+class C19APIUserProfileAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(C19APIUserProfile, C19APIUserProfileAdmin)
--- c19-backend/api/models.py
+++ c19-backend/api/models.py
@@ -1,3 +1,13 @@
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+class C19APIUserProfile(models.Model):
+    user = models.OneToOneField(
+        settings.AUTH_USER_MODEL,
+        related_name='c19_api_user_profile',
+        on_delete=models.CASCADE)
+    clinical_author_name = models.TextField()
+    clinical_author_id = models.TextField()
+
+    def __str__(self):
+        return str(self.user)
