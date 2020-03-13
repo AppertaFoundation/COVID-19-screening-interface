c096ecba3f6823ae1c7910c258eebb9faad050a8
adabedi <adrianna.bedychaj@gmail.com>
2020-03-13 14:40:47 +0000

    [ADD] Added warining to intro page

--- frontend/src/common/Pages/Intro/index.js
+++ frontend/src/common/Pages/Intro/index.js
@@ -2,7 +2,28 @@ import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import { Link } from 'react-router-dom';
 import { Grid } from '@material-ui/core';
+import { Snackbar } from '@material-ui/core';
+import MuiAlert from '@material-ui/lab/Alert';
 
+function Alert(props) {
+  return <MuiAlert elevation={6} variant="filled" {...props} />;
+}
+const Warinning = () => {
+  const [open, setOpen] = React.useState(true);
+  const handleClose = () => setOpen(false);
+
+  return (
+    <Snackbar
+      open={open}
+      onClose={handleClose}
+      anchorOrigin={{ vertical: 'top', horizontal: 'center' }}
+    >
+      <Alert onClose={handleClose} severity="warning">
+        Wipe screen before use
+      </Alert>
+    </Snackbar>
+  );
+};
 const useStyles = makeStyles(theme => ({
   root: {
     height: '100vh',
@@ -21,7 +42,8 @@ export default () => {
       alignItems="center"
       spacing={6}
     >
-      <Link to="/sign-in">Login</Link>
+      <Warinning />
+      <Link to="/sign-in">Let's start</Link>
     </Grid>
   );
 };
