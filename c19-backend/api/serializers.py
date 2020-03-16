91f2a6c49d116042134ffc9f19b44ff284ea1c98
Nick Booker <nick.booker@opusvl.com>
2020-03-16 14:44:56 +0000

    Remove serializers we probably won't be using
    We can always get them back if we do need them

--- c19-backend/api/serializers.py
+++ c19-backend/api/serializers.py
@@ -2,35 +2,3 @@
 from rest_framework import serializers
 
 
-class BodyTempSerializer(serializers.Serializer):
-    magnitude = serializers.CharField()
-    units = serializers.CharField()
-
-
-class OtherSymptomSerializer(serializers.Serializer):
-    symptomNameText = serializers.CharField()
-    symptomNameCode = serializers.CharField()
-    symptomNameTerminology = serializers.CharField()
-
-
-class SymptomsSerializer(serializers.Serializer):
-    firstSymptomsPresenceCode = serializers.CharField()
-    firstSymptomsPresenceText = serializers.CharField()
-    dateOfOnset = serializers.CharField()
-    coughPresenceCode = serializers.CharField()
-    coughPresenceText = serializers.CharField()
-    feverPresenceCode = serializers.CharField()
-    feverPresenceValue = serializers.CharField()
-    difficultyBreathingPresenceCode = serializers.CharField()
-    difficultyBreathingPresenceText = serializers.CharField()
-    soreThroatPresenceCode = serializers.CharField()
-    soreThroatPresenceText = serializers.CharField()
-    otherSymptom = OtherSymptomSerializer()
-
-
-class Covid19Serializer(serializers.Serializer):
-    clinicalAuthorName = serializers.CharField()
-    clinicalAuthorId = serializers.CharField()
-    documentTime = serializers.CharField()
-    symptoms = SymptomsSerializer()
-    bodyTemp = BodyTempSerializer()
--- c19-backend/api/views.py
+++ c19-backend/api/views.py
@@ -1,5 +1,4 @@
 from django.http import Http404
-from api.serializers import Covid19Serializer
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
