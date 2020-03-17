a535e4fc387c2791062d988d9fbace10495422de
Nick Booker <nick.booker@opusvl.com>
2020-03-17 15:35:17 +0000

    Fetch the NHS id of the patient

--- c19-backend/api/views.py
+++ c19-backend/api/views.py
@@ -8,7 +8,22 @@ from . import ehrbase
 class CovidScreenListView(APIView):
 
     def post(self, request, format=None):
-        # serializer = Covid19Serializer(data=request.data)
-        # if serializer.is_valid():
-        #     ehrbase.CONNECTION.post(data=serializer.data)
-        return Response(data={'fakeresponse': 'foo'})
+        if (patient := request.user.c19_api_patient_profile):
+            # serializer = Covid19Serializer(data=request.data)
+            # if serializer.is_valid():
+            #     ehrbase.CONNECTION.post(data=serializer.data)
+            return Response(
+                data={
+                    # TODO we probably won't send the nhs number back,
+                    #  this is just for stubbing to check the code branches
+                    'nhs_number': patient.patient_nhs_number,
+                },
+            )
+        else:
+            return Response(
+                data={
+                    'status': 'Unauthorized',
+                    'error': 'No patient profile record for user',
+                },
+                status=401,
+            )
